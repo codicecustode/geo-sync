e5d8845700ecd56b4c35e5b70a4751197e135f1c
amaninvestationteam <amanrkt2020@gmail.com>
2026-02-27 12:00:23 +0530

    feat(server): add health check endpoint to confirm server status

--- server/src/server.js
+++ server/src/server.js
@@ -8,10 +8,14 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: { origin: "*" }
+  cors: { origin: "*" },
 });
 
 socketHandler(io);
+
+app.use("/", (_, res) => {
+  return res.send("server is running");
+});
 
 server.listen(5000, () => {
   console.log("Server running");
